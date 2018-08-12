f1cebcfd1e24993807fefa266d67907f720b5958
Veovis <veovis@kveer.fr>
2018-08-12 02:21:05 +0200

    bump version to 1.42.01 fix script for php to access system environment variables update the installed dependencies use mariadb instead of mysql transfer the sql-mode parameter directly to mariadb

--- Dockerfile
+++ Dockerfile
@@ -1,37 +1,48 @@
-FROM alpine:3.6
+FROM alpine:3.8
 
-ARG VERSION=1.33
+ARG VERSION=1.42.01
 ARG librenms_base=/opt/librenms
 LABEL version="${VERSION}" \
 	description="librenms container with alpine" \
 	maintainer="Veovis <veovis@kveer.fr>"
 
+# install required packages
 RUN apk upgrade --no-cache && \
 	apk add --no-cache \
+	acl \
 	ca-certificates \
 	openssl \
 	tzdata \
 	bash \
+	git \
 	php7 \
 	php7-ctype \
 	php7-curl \
 	php7-fpm \
 	php7-gd \
+	php7-iconv \
 	php7-json \
+	php7-mbstring \
 	php7-mcrypt \
 	php7-memcached \
 	php7-mysqli \
 	php7-openssl \
+	php7-phar \
+	php7-pdo \
+	php7-pdo_mysql \
 	php7-pear \
+	php7-posix \
 	php7-session \
 	php7-simplexml \
 	php7-snmp \
+	php7-tokenizer \
 	net-snmp \
 	net-snmp-tools \
 	graphviz \
 	nginx \
 	fping \
 	imagemagick \
+	sudo \
 	whois \
 	nmap \
 	rrdtool \
@@ -39,30 +50,37 @@ RUN apk upgrade --no-cache && \
 	py-mysqldb \
 	runit \
 	dcron \
-	mysql-client \
-	mtr && \
-	update-ca-certificates && \
-	pear channel-update pear.php.net && \
-	pear install pear/Net_IPv4 && \
-	pear install pear/Net_IPv6 && \
-	mkdir -p /opt
+	mariadb-client \
+	util-linux \
+	mtr
+
+# download librenms
+RUN set -e; \
+	mkdir /opt; \
+	git clone https://github.com/librenms/librenms.git --branch ${VERSION} --depth=1 --single-branch "$librenms_base"
+
+# post-install system
+RUN set -e; \
+	update-ca-certificates; \
+	ln -s fping /usr/sbin/fping6; \
+	pear channel-update pear.php.net; \
+	pear install pear/Net_IPv4; \
+	pear install pear/Net_IPv6
 
 RUN	rm /etc/nginx/conf.d/default.conf && \
 	sed -i -e 's/^;pid/pid/' /etc/php7/php-fpm.conf && \
 	sed -i -e 's!^; \?include_path.*!include_path=".:/usr/share/php7"!' /etc/php7/php.ini && \
+	sed -i -e 's/^\(variables_order\).*/\1="EGPCS"/' /etc/php7/php.ini && \
 	rm /etc/php7/php-fpm.d/www.conf
 
-RUN	wget -q -O - -c "https://github.com/librenms/librenms/archive/${VERSION}.tar.gz" | tar -zx -f - -C /opt && \
-	mv ${librenms_base}-${VERSION} $librenms_base && \
-	sed -i -e 's/ALTER IGNORE TABLE/ALTER TABLE/' $librenms_base/sql-schema/109.sql && \
-	sed -i -e 's/ALTER IGNORE TABLE/ALTER TABLE/' $librenms_base/sql-schema/181.sql && \
-	adduser -D -h $librenms_base librenms && \
+WORKDIR $librenms_base
+
+RUN adduser -D -h $librenms_base librenms && \
 	adduser nginx librenms && \
-	install -m 775 -d $librenms_base/rrd && \
-	install -d $librenms_base/logs && \
+	install -m 775 -d $librenms_base/bootstrap/cache $librenms_base/cache $librenms_base/logs $librenms_base/rrd $librenms_base/storage $librenms_base/vendor && \
 	chown -R librenms:librenms $librenms_base && \
 	cp $librenms_base/snmpd.conf.example /etc/snmp/snmpd.conf && \
-	cp $librenms_base/config.php.default $librenms_base/config.php && \
+	install -o librenms -g librenms $librenms_base/config.php.default $librenms_base/config.php && \
 	echo "\$config['fping'] = '/usr/sbin/fping';" >> $librenms_base/config.php && \
 	echo "if(file_exists(realpath(__DIR__) . '/config.d/config.php')) include realpath(__DIR__) . '/config.d/config.php';" >> $librenms_base/config.php && \
 	echo "if(file_exists(realpath(__DIR__) . '/config.d/_memcache.php')) include realpath(__DIR__) . '/config.d/_memcache.php';" >> $librenms_base/config.php && \
@@ -72,10 +90,8 @@ RUN	wget -q -O - -c "https://github.com/librenms/librenms/archive/${VERSION}.tar
 	sed -i -e 's/ librenms //' /etc/crontabs/librenms && \
 	sed -i 's/^#\($config\['"'"'update'"'"'\].*\)/\1/' "$librenms_base/config.php"
 
-COPY patches /tmp/patches
-WORKDIR $librenms_base
-RUN patch -p 1 -i /tmp/patches/002_missing_menu_index.patch && \
-	patch -p 1 -i /tmp/patches/003_missing_used_sensors_index.patch
+# install php dependencies with composer
+RUN sudo -u librenms ./scripts/composer_wrapper.php install --no-dev --no-scripts
 
 COPY services /etc/sv
 COPY docker-entrypoint.sh /
@@ -86,5 +102,4 @@ ENTRYPOINT [ "/docker-entrypoint.sh" ]
 CMD [ "runsvdir", "-P /etc/sv" ]
 
 EXPOSE 80
-WORKDIR /
-VOLUME ["$librenms_base/logs", "$librenms_base/rrd", "$librenms_base/config.d"]
+VOLUME ["$librenms_base/logs", "$librenms_base/rrd", "$librenms_base/storage"]
--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -6,28 +6,37 @@ echo "$TIMEZONE" >> /etc/timezone
 
 # setting MYSQL_PASS from MYSQL_PASS_FILE if set
 if [ -f "$MYSQL_PASS_FILE" ]; then
-	MYSQL_PASS=$(cat "$MYSQL_PASS_FILE")
+	export DB_PASSWORD=$(cat "$MYSQL_PASS_FILE")
 fi
 
 librenms_base=/opt/librenms
 librenms_config="$librenms_base/config.php"
 
+# to assure compatibility
+[ ! -f "$librenms_base/.env" ] && sudo -u librenms touch "$librenms_base/.env"
+sed -i -e '/^\(DB_|APP\)/d' -e '/^APP_/d' "$librenms_base/.env"
+env | grep -E '(DB_|APP)' >> "$librenms_base/.env"
+
+# keeping the old configuration until all code is upgraded
+sed -i -e "s/^\(\$config\['db_host'\]\).*/\1 = '$DB_HOST';/" "$librenms_config"
+sed -i -e "s/^\(\$config\['db_user'\]\).*/\1 = '$DB_USERNAME';/" "$librenms_config"
+sed -i -e "s/^\(\$config\['db_pass'\]\).*/\1 = '$DB_PASSWORD';/" "$librenms_config"
+sed -i -e "s/^\(\$config\['db_name'\]\).*/\1 = '$DB_DATABASE';/" "$librenms_config"
+
 # setting mysql access for librenms
 sed -i -e "s!^;\?\(date.timezone\).*!\1 = $TIMEZONE!" /etc/php7/php.ini
-sed -i -e "s/^\(\$config\['db_host'\]\).*/\1 = '$MYSQL_HOST';/" "$librenms_config"
-sed -i -e "s/^\(\$config\['db_user'\]\).*/\1 = '$MYSQL_USER';/" "$librenms_config"
-sed -i -e "s/^\(\$config\['db_pass'\]\).*/\1 = '$MYSQL_PASS';/" "$librenms_config"
-sed -i -e "s/^\(\$config\['db_name'\]\).*/\1 = '$MYSQL_NAME';/" "$librenms_config"
 
 # settings the memcache
 librenms_config_memcache="$librenms_base/config.d/_memcache.php"
 if [ ! -z "MEMCACHE_HOST" ]; then
+	install -d -o librenms -g librenms `dirname $librenms_config_memcache`
 	cat > "$librenms_config_memcache" <<EOF
 <?php
 \$config['memcached']['enable'] = true;
 \$config['memcached']['host'] = '$MEMCACHE_HOST';
 \$config['memcached']['port'] = 11211;
 EOF
+	chown librenms:librenms "$librenms_config_memcache"
 else
 	[ -f "$librenms_config_memcache" ] && rm "$librenms_config_memcache"
 fi
@@ -36,7 +45,7 @@ fi
 
 # waiting for mysql to be up
 # https://stackoverflow.com/a/37300565/878381
-until nc -z -v -w30 $MYSQL_HOST 3306
+until nc -z -v -w30 $DB_HOST 3306
 do
   echo "Waiting 5 seconds for mysql to be ready..."
   # wait for 5 seconds before check again
@@ -44,37 +53,30 @@ do
 done
 
 # initializes the database if empty
-mysql_cmd="mysql -h $MYSQL_HOST -u "$MYSQL_USER" "-p$MYSQL_PASS" -B"
+mysql_cmd="mysql -h $DB_HOST -u "$DB_USERNAME" "-p$DB_PASSWORD" -B"
 
 # unsetting sensitive variables
-unset MYSQL_PASS
 unset MYSQL_PASS_FILE
 
 # exporting variables to be availables to services
 export > /etc/envvars
 [ ! -f /etc/envvars ] && echo "/etc/envvars does not exists" && exit
 
-tables=$(echo 'SHOW TABLES' | $mysql_cmd "$MYSQL_NAME" | wc -l)
+tables=$(echo 'SHOW TABLES' | $mysql_cmd --database "$DB_DATABASE" | wc -l)
 if [ "$tables" -eq '0' ]; then
 
-	# as of 1.26, librenms can't use MySQL with STRICT_TRANS_TABLES, which is included by default in mysql 5.7
-	# this piece of code temporary remove the STRICT_TRANS_TABLES from sql_mode during the database creation
-	sql_mode=$($mysql_cmd -e 'select @@GLOBAL.sql_mode;' | tail -n 1)
-	if [ "${sql_mode#*STRICT_TRANS_TABLES}" != "$sql_mode" ]; then
-		new_sql_mode="SET SESSION sql_mode = '$(echo $sql_mode | sed -e 's/STRICT_TRANS_TABLES//' -e 's/,,/,/')';"
-	else
-		new_sql_mode=
-	fi
-
 	echo Creating initial sql schema
-	(echo $new_sql_mode; cat "$librenms_base/build.sql") | $mysql_cmd "$MYSQL_NAME"
+	(echo $new_sql_mode; cat "$librenms_base/build.sql") | $mysql_cmd --database "$DB_DATABASE"
 
 	# it seems the order is alphabetical
 	# i'm note very sure of that certainty
 	for i in `ls "$librenms_base"/sql-schema/*.sql | sort`; do
 		echo Updating schema with $i
-		(echo $new_sql_mode; cat "$i") | $mysql_cmd "$MYSQL_NAME"
+		(echo $new_sql_mode; cat "$i") | $mysql_cmd --database "$DB_DATABASE"
 	done
+
+	$mysql_cmd --database "$DB_DATABASE" -e 'ALTER TABLE `ports_fdb` DROP INDEX `mac_address_2`' 2> /dev/null
+	$mysql_cmd --database "$DB_DATABASE" -e 'ALTER TABLE `users` DROP `remeber_token`' 2> /dev/null
 
 	# creating the first user
 	echo Creating the first user
@@ -82,5 +84,13 @@ if [ "$tables" -eq '0' ]; then
 	php adduser.php librenms librenms 10 admin@librenms.local
 	echo Your credentials: librenms / librenms
 fi
+
+cd "$librenms_base"
+
+# run the post-install scripts
+sudo -u librenms ./scripts/composer_wrapper.php install --no-dev
+
+# update the database schema
+sudo -u librenms ./daily.sh
 
 exec $@
